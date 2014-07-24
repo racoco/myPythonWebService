e33c18b8b3f0563efe57a90e0e2974e1307bbcdd
racoco <racoco@gmail.com>
2014-07-24 16:39:02 -0400

    add commments

--- uwsgiHelloWorldExample.py
+++ uwsgiHelloWorldExample.py
@@ -1,4 +1,7 @@
+
+# To run this with the uWSGI binary:
+#    uwsgi --http :9090 --wsgi-file uwsgiHelloWorldExample.py
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World"]
+    return ["Hello World"]
     
