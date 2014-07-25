b3bb57298d2e5f789fa2e9b1b44fa2cce081a918
racoco <racoco@gmail.com>
2014-07-25 09:32:34 -0400

    add commments

--- uwsgiHelloWorldExample.py
+++ uwsgiHelloWorldExample.py
@@ -1,6 +1,10 @@
 
 # To run this with the uWSGI binary:
 #    uwsgi --http :9090 --wsgi-file uwsgiHelloWorldExample.py
+# See: https://uwsgi-docs.readthedocs.org/en/latest/WSGIquickstart.html
+#
+# NOTE: The above command must be run in the same directory in which uwsgi was built. Why?
+
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
     return ["Hello World"]
